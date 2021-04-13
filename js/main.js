9f28f1720642765551a90d10a23ca610a8d7fbfa
TsubasaTakeda <tsubasa.takeda.t5@dc.tohoku.ac.jp>
2021-04-13 21:00:37 +0900

    動的に内容を変えることに成功

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@
 document.getElementById("english_button").onclick = function() {
     window.alert("404 Error.\nこんなページはないよ！");
 }
+
+function initialize_common_section(){
+    var id = document.getElementById('return');
+    
+    id.innerHTML = '<nav><ul><li><a href="#header">Topに戻る</a></li><li><a href="#" id="english_button">English</a></li></ul></nav>'
+}
+
+initialize_common_section();
