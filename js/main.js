b175e350a705207d04c3581d14bd0d16d9352e8d
TsubasaTakeda <80091069+TsubasaTakeda@users.noreply.github.com>
2021-04-25 19:02:38 +0900

    study reading json_file

--- js/main.js
+++ js/main.js
@@ -10,4 +10,7 @@ function initialize_common_section(){
     id.innerHTML = '<nav><ul><li><a href="#header">Topに戻る</a></li><li><a href="#" id="english_button">English</a></li></ul></nav>'
 }
 
+
+
 initialize_common_section();
+
