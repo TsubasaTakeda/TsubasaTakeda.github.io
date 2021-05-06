dd3c5c03dbb58801a326ef0ef781258a904a64fe
TsubasaTakeda <tsubasa.takeda.t5@dc.tohoku.ac.jp>
2021-05-07 05:12:45 +0900

    revise json_setting

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-
-
-document.getElementById("english_button").onclick = function() {
-    window.alert("404 Error.\nこんなページはないよ！");
-}
-
 function initialize_common_section(){
+
+    news_url = 'json/news/2021/news_2021.json';
+    $.getJSON(news_url, function getNews(data) {
+        var news_id = document.getElementById('news_contents');
+        var news_data = data;
+        news_id.innerHTML = '';
+        for (var i = 0; i < 2; i++) {
+            date = '<dt>' + news_data[i].date + '</dt>';
+            contents = '<dd>' + news_data[i].contents + '</dd>';
+            news_id.innerHTML += date + contents;
+        }
+        news_id.innerHTML += '</dl>';
+        console.log(news_id.innerHTML);
+    });
+
+
     var id = document.getElementById('return');
-    
+
     id.innerHTML = '<nav><ul><li><a href="#header">Topに戻る</a></li><li><a href="#" id="english_button">English</a></li></ul></nav>'
+
+    document.write('最終更新日：' + document.lastModified);
+
 }
-
-
 
 initialize_common_section();
+
+
+document.getElementById("english_button").onclick = function () {
+    window.alert("404 Error.\nこんなページはないよ！");
+}
 
