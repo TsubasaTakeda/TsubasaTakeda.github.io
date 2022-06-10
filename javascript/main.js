31ec6f5205d855bab26fd110f05b0bdbbd42e844
TsubasaTakeda <tsubasa.takeda.t5@dc.tohoku.ac.jp>
2022-06-10 16:05:18 +0900

    スタイルガイドを作成

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+const ol_id = document.querySelector("#ol_rev");
+ol_id.style.counterReset = "ol 10";
+console.log(ol_id.style.counterReset);
+console.log(ol_id.getAttribute("counter-reset"));
+console.log(ol_id.getAttributeNames());
+// console.log(ol_id);
